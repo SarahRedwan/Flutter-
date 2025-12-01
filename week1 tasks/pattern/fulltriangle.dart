f116c9e8f640d8ac6cb8211d35f7968045a99795
Sara Redwan Shure <susured22@gmail.com>
2025-12-01 09:34:02 +0300

    Update fulltriangle.dart

--- week1 tasks/pattern/fulltriangle.dart
+++ week1 tasks/pattern/fulltriangle.dart
@@ -1,14 +1,17 @@
+import 'dart:io';
+
 void main() {
   int rows = 5;
 
   for (int i = 1; i <= rows; i++) {
     // Print spaces
     for (int s = 1; s <= rows - i; s++) {
-      print(' ', terminator: '');
+      stdout.write(' ');
     }
+
     // Print stars
     for (int j = 1; j <= (2 * i - 1); j++) {
-      print('*', terminator: '');
+      stdout.write('*');
     }
     print('');
   }
