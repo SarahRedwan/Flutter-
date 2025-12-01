06d2be74ecb09ea814546acea56e3b64c16419ad
Sara Redwan Shure <susured22@gmail.com>
2025-12-01 09:22:35 +0300

    Create function.dart

---
+++ week1 tasks/function.dart
@@ -0,0 +1,8 @@
+void main() {
+  printUserInfo("Sara", 20); // You can change the values
+}
+
+void printUserInfo(String name, int age) {
+  print("User Name: $name");
+  print("User Age: $age");
+}
